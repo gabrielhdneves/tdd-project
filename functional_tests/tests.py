1e559a5a310cad8b04c9f429e11f337ec9dbe0e7
Gabriel Neves <bielhenrique1999@gmail.com>
2022-01-31 17:37:01 -0300

    Isolando Testes Funcionais

---
+++ functional_tests/tests.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+        def test_can_start_a_list_and_retrieve_it_later(self): 
+        
+            # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+            self.browser.get(self.live_server_url)
+            # Quando ela aperta enter, a página atualiza, e mostra a lista
+            # "1: Estudar testes funcionais" como um item da lista TODO
+            inputbox.send_keys(Keys.ENTER)
+            time.sleep(1)
+            self.check_for_row_in_list_table('1: Estudar testes funcionais')
+            
+            # Ainda existe uma caixa de texto convidando para adicionar outro item
+            # Ela digita: "Estudar testes de unidade"
+            inputbox = self.browser.find_element_by_id('id_new_item')  
+            inputbox.send_keys('Estudar testes de unidade')
+            inputbox.send_keys(Keys.ENTER)
+            time.sleep(1)
+
+            # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+            self.check_for_row_in_list_table('1: Estudar testes funcionais')
+            self.check_for_row_in_list_table('2: Estudar testes de unidade')
+
+            # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+            # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+            # Ela visita a URL: a sua lista TODO ainda está armazenada
+
+            # Satisfeita, ela vai dormir
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+if __name__ == '__main__':
+    unittest.main()

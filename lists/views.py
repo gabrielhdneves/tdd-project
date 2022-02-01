dd6320ea59a21ddad6df6dcfedeec86f49ecfd35
Gabriel Neves <bielhenrique1999@gmail.com>
2022-01-31 23:31:46 -0300

    Implementando o Novo Projeto: Ajustando o Resto do Sistema

--- lists/views.py
+++ lists/views.py
@@ -9,5 +9,6 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    my_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=my_list)
     return redirect('/lists/the-only-list-in-the-world/')

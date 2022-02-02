d6a543ea7f5929c4d604cccdc81d15e22cb2a387
Gabriel Neves <bielhenrique1999@gmail.com>
2022-02-02 20:48:56 -0300

    Modelo Token

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from accounts.models import Token
+import uuid
 
 class User(models.Model):
     email = models.EmailField(primary_key=True)
@@ -7,3 +9,14 @@ class User(models.Model):
     USERNAME_FIELD = 'email'
     is_anonymous = False
     is_authenticated = True
+
+class TokenModelTest(TestCase):
+
+    def test_links_user_with_auto_generated_uid(self):
+        token1 = Token.objects.create(email='a@b.com')
+        token2 = Token.objects.create(email='a@b.com')
+        self.assertNotEqual(token1.uid, token2.uid)
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)

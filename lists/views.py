27459031df4bcefce0b5d88408ba3384eb51ae02
Gabriel Neves <bielhenrique1999@gmail.com>
2022-02-02 19:24:52 -0300

    Validando os erros na view

--- lists/views.py
+++ lists/views.py
@@ -9,9 +9,13 @@ def view_list(request, list_id):
     return render(request, 'list.html', {'list': my_list})
 
 def new_list(request):
-    my_list = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=my_list)
-    return redirect(f'/lists/{my_list.id}/')
+    list_ = List.objects.create()
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        return render(request, 'home.html')
+    return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)

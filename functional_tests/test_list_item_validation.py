11cb350b1ef8096102f109563c0fba9a272c8eb3
Gabriel Neves <bielhenrique1999@gmail.com>
2022-02-02 18:56:24 -0300

    Validação: Prevenindo itens vazios - PARTE 1

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,47 @@
+from selenium.webdriver.common.keys import Keys
+from unittest import skip
+from .base import FunctionalTest
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_list_items(self):
+
+        # Edith vai na home page e acidentalmente submete um item vazio
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # A página atualiza e apresenta uma mensagem de erro
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
+
+        # Ela adiciona alguns itens reais
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Ela decide submeter uma lista vazia novamente
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # A página atualiza e apresenta uma mensagem de erro
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
+
+        # E ela pode corrigir o erro, adicionando algum item correto
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()  
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
